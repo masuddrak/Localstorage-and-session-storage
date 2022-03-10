2a6306e9a228a98129083975a996c7da0ca12331
masud rana <masudranaacb@gmail.com>
2022-03-09 20:45:36 -0800

    event using

---
+++ app.js
@@ -0,0 +1,7 @@
+const myBtn=()=>{
+    const inputItem=document.getElementById('inputItem')
+    const inputAmount=document.getElementById('inputAmount')
+    const inputItemValue=inputItem.value
+    const inputAmountValue=inputAmount.value
+    console.log(inputAmountValue)
+}
